bc0d2958c816c926c8acf0333064575546f5708b
Alex Younger <thealexyounger@proton.me>
2024-09-26 01:29:03 -0500

    docs: add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# LlamaPy
+
+## Talk to local AI models using OpenAI's API
+
+Inspired by [llamanet](https://github.com/pinokiocomputer/llamanet), but simplified and written in only Python.
+
+A wrapper for the Python OpenAI API that allows you to talk to local GGUF models on your machine
+
+This is still a work in progress but should be finished soon. Check back later. 
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='local-llm-api',
+    name='llamapy',
     version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Alex Younger',
+    author_email='thealexyounger@proton.me',
     description='A local API for LLMs',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/local-llm-api',
+    url='https://github.com/AlextheYounga/llamapy',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
@@ -17,9 +17,9 @@ setup(
     ],
     python_requires='>=3.11',
     install_requires=[
-        'requests',
-		'fake-useragent',
-		'aiohttp',
-		'fastapi'
+		'requests'
+		'gputil'
+		'openai'
+		'tqdm'
     ],
 )
